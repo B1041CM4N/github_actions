21aeccf9d2a01fc5d4065aa61a9ad518b55c693c
B1041CM4N <rodriguezch_ignacioandres@hotmail.cl>
2023-02-07 20:15:51 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
 console.log("I'm from node")
 console.log(`COURSE_ID: ${process.env.COURSE_ID}`)
 console.log(`DOCUMENTATION_URL: ${process.env.DOCUMENTATION_URL}`)
+console.log(`GITHUB_USERNAME: ${process.env.GITHUB_USERNAME}`)
